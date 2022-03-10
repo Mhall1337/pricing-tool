71f0d239078d5e6aabe60cefccc34c5a258637b6
Mitchell <Hmitchell.hall@gmail.com>
2022-03-10 11:45:13 -0600

    generated resources for Dispatcher model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_10_174128) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_10_174424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carriers", force: :cascade do |t|
     t.string "carrier_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dispatchers", force: :cascade do |t|
+    t.string "dispatcher_name"
+    t.integer "dispatcher_phone_number"
+    t.string "email"
+    t.integer "carrier_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
