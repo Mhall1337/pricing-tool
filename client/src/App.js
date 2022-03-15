d01481f30e7b8323c7d0fe53c589149acaff7c68
Mitchell <Hmitchell.hall@gmail.com>
2022-03-15 15:24:36 -0500

    added form to Login component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Login from './Components/Login';
+function App() {
+  return (
+    <div className="App">
+      <h3>Build Sign Up & Login UI Template in React</h3>
+      <Login />
+    </div>
+  );
+}
+export default App;
